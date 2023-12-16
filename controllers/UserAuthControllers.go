431b66435e83b5cd6e8249b653ee2fe8f34141f5
Jonathan Nguyen <jnganguyen3@gmail.com>
2023-12-16 12:21:42 -0600

    changed environment variable

--- controllers/UserAuthControllers.go
+++ controllers/UserAuthControllers.go
@@ -18,6 +18,7 @@ import (
 )
 
 var smtpPass string = os.Getenv("SMTP_PASS")
+var backendUrl string = os.Getenv("KNOSTASH_BACKEND_URL")
 var jwtSecret []byte = []byte(os.Getenv("JWT_SECRET"))
 
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
@@ -77,7 +78,7 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
   toMsg := fmt.Sprintf("To: %v\n", data.Email)
   subject := "Subject: Test email from Go!\n"
   mime := "MIME-version: 1.0; \nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-  body := fmt.Sprintf("<html><body>Verify your account at this link: http://%v/auth/verifyaccount/%v</body></html>", r.Host, ss)
+  body := fmt.Sprintf("<html><body>Verify your account at this link: http://%v/auth/verifyaccount/%v</body></html>", backendUrl, ss)
   msg := []byte(fromMsg + toMsg + subject + mime + body)
 
   // Configuration for sending an email
--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,4 @@
 export SMTP_PASS=XXXXXX
 export DB_CONN=XXXXXX
 export JWT_SECRET=XXXXXX
-export BACKEND_URL=localhost
+export KNOSTASH_BACKEND_URL=http://localhost:8000
