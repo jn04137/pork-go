e2b2d15cb28d5b2cf9654f463bc902ecc4646254
Jonathan Nguyen <jnganguyen3@gmail.com>
2023-12-17 01:23:29 -0600

    finalizing some changes

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,6 +4,6 @@ docker build -t wthunder/pork-go .
 docker run -p 8000:8000 \
   -e SMTP_PASS=${SMTP_PASS} \
   -e DB_CONN=${DB_CONN} \
-  -e JWT_SECRET=${JWT_SECRET} \ 
-  -e KNOSTASH_BACKEND_URL=${KNOSTASH_BACKEND_URL} \ 
+  -e JWT_SECRET=${JWT_SECRET} \
+  -e KNOSTASH_BACKEND_URL=${KNOSTASH_BACKEND_URL} \
   wthunder/pork-go
--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,4 @@
 export SMTP_PASS=XXXXXX
-export DB_CONN=XXXXXX
-export JWT_SECRET=XXXXXX
+export DB_CONN=postgres://XXXXXX:changeme@postgres:5432/trust_me?sslmode=disable
+export JWT_SECRET=testhash
 export KNOSTASH_BACKEND_URL=http://localhost:8000
