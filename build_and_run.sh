11ace48644cc07e21a9ca63a55310f1822b68cd7
Jonathan Nguyen <jnganguyen3@gmail.com>
2023-12-17 08:29:56 +0000

    passing host port to container

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t wthunder/pork-go .
-docker run -p 8000:8000 \
+#docker build -t wthunder/pork-go .
+docker run -ti --net=host -p 8000:8000 \
   -e SMTP_PASS=${SMTP_PASS} \
   -e DB_CONN=${DB_CONN} \
   -e JWT_SECRET=${JWT_SECRET} \
